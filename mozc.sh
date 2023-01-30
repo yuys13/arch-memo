021fde35f6587eb87aba6109996b06e3b61532d5
yuys13 <yuys13@users.noreply.github.com>
2023-01-30 23:41:00 +0900

    feat: add fcitx5-mozc

---
+++ mozc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ue pipefail
+
+sudo pacman -S fcitx5-mozc fcitx5-qt fcitx5-gtk fcitx5-configtool
+
+cat <<EOF >>~/.xprofile
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+EOF
+
+if [[ -f ~/.config/i3/config ]]; then
+	echo exec --no-startup-id fcitx5 -d >>~/.config/i3/config
+fi
