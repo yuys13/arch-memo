83a086af14492bf083a1535dd1bf6355004e8e82
yuys13 <yuys13@users.noreply.github.com>
2023-02-02 00:49:38 +0900

    feat: add rofi conf

--- i3.sh
+++ i3.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
+I3CONFDIR="$HOME/.config/i3"
+ROFICONFDIR="$HOME/.config/rofi"
+
 set -ue pipefail
 
 sudo pacman -S rofi arandr
 
 echo export TERMINAL=alacritty >>~/.xprofile
 
-if [[ ! -d ~/.config/i3 ]]; then
-	mkdir -p ~/.config/i3
-	chmod 700 ~/.config/i3
+if [[ ! -d $I3CONFDIR ]]; then
+	mkdir -p "$I3CONFDIR"
+	chmod 700 "$I3CONFDIR"
 fi
 
-if [[ -f ~/.config/i3/config ]]; then
-	cp ~/.config/i3/config ~/.config/i3/config.bak
+if [[ ! -d $ROFICONFDIR ]]; then
+	mkdir -p "$ROFICONFDIR"
+	chmod 700 "$ROFICONFDIR"
+fi
+
+if [[ -f $I3CONFDIR/config ]]; then
+	cp "$I3CONFDIR"/config "$I3CONFDIR"/config.bak
 fi
 
 BASEDIR=$(dirname "$0")
-cp "${BASEDIR}"/i3/config ~/.config/i3/config
+cp "${BASEDIR}"/i3/config "$I3CONFDIR"/config
+cp "${BASEDIR}"/rofi/config.rasi "$ROFICONFDIR"/config.rasi
 
 echo i3/config has been updated. Please check the diff with the following command:
-echo diff ~/.config/i3/config.bak ~/.config/i3/config
+echo diff "$I3CONFDIR"/config.bak "$I3CONFDIR"/config
 echo Create ~/.screenlayout/defaults.sh using the arandr.
